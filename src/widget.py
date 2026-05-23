bc10a858f2501b8779eede5ae7e14245fcba0d8f
svmikhaylov333 <svmikhaylov333@gmail.com>
2026-05-23 22:09:59 +0300

    add widget.py

---
+++ src/widget.py
@@ -0,0 +1,59 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+# def get_mask_card_number(card_number: str) -> str:
+#     """Маскирует номер банковской карты - XXXX XX** **** XXXX"""
+#
+#     card_number = str(card_number).replace(" ", "")
+#     if len(card_number) != 16 or not card_number.isdigit():
+#         return "Неверный номер карты"
+#     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+#
+#
+# def get_mask_account(account: str) -> str:
+#     """Маскирует номер банковского счета - **XXXX"""
+#
+#     account = str(account).replace(" ", "")
+#     if len(account) < 4 or not account.isdigit():
+#         return "Неверный номер счета"
+#     return f"**{account[-4:]}"
+#
+
+
+def mask_account_card(card_or_account_info: str) -> str:
+    """Маскирует информацию о карте или счете
+    Примеры:
+    "Visa Platinum 7000792289606361" -> "Visa Platinum 7000 79** **** 6361"
+    "Счет 73654108430135874305" -> "Счет **4305"
+    """
+
+    parts = card_or_account_info.rsplit(" ", 1)
+
+    if len(parts) != 2:
+        return "Введены неверные данные"
+
+    type_card_or_account = parts[0]
+    number = parts[1]
+
+    if not number.isdigit():
+        return "Введены неверные данные"
+
+    if "Счет" in type_card_or_account:
+        return f"{type_card_or_account} {get_mask_account(number)}"
+    else:
+        return f"{type_card_or_account} {get_mask_card_number(number)}"
+
+
+def get_date(date: str) -> str:
+    """меняет формат даты 2024-03-11T02:26:18.671407 в "ДД.ММ.ГГГГ" ("11.03.2024")"""
+    if len(date) < 10:
+        return "Неверный формат даты"
+    date_part = date[:10]
+    if "-" not in date_part:
+        return "Неверный формат даты"
+    # parts = date_part.split("-")
+    # if len(parts) !=3:
+    #     return "Введены неверные данные"
+    # else:
+    #     return f"{parts[2]}.{parts[1]}.{parts[0]}"
+    year, month, day = date_part.split("-")
+    return f"{day}.{month}.{year}"
--- test/test.py
+++ test/test.py
@@ -1,8 +1,46 @@
 from src.masks import get_mask_card_number, get_mask_account
+from src.widget import  mask_account_card, get_date
+
+print("=" * 50)
+print("проверка get_mask_card_number")
+print("=" * 50)
 
 print(get_mask_card_number("1234 5678 9123 456q"))
 print(get_mask_card_number("1234 5678 9123 456"))
-
 print(get_mask_card_number("1234 5678 9123 4567"))
+
+print("=" * 50)
+print("проверка get_mask_account")
+print("=" * 50)
+
 print(get_mask_account("123"))
 print(get_mask_account("1234 5678 9123 4567"))
+
+print("=" * 50)
+print("проверка get_date")
+print("=" * 50)
+
+print(get_date("1234 5678 9123 4567"))
+print(get_date("2024-03-11T02:26:18.671407"))
+
+print("=" * 50)
+print("проверка mask_account_card")
+print("=" * 50)
+
+test_data = [
+    "Maestro 1596837868705199",
+    "Счет 64686473678894779589",
+    "MasterCard 7158300734726758",
+    "Счет 35383033474447895560",
+    "Visa Classic 6831982476737658",
+    "Visa Platinum 8990922113665229",
+    "Visa Gold 5999414228426353",
+    "Счет 73654108430135874305",
+    "Счеafhbaehafh54108430135874305",
+    "Счеaf r r ",
+]
+
+for item in test_data:
+    result = mask_account_card(item)
+    print(f"{item:<35} -> {result}")
+    # print(f"{item} -> {result}")
