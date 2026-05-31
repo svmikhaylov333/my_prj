a4535075c7722b3498010e447e5f9fca5e78a5d2
svmikhaylov333 <svmikhaylov333@gmail.com>
2026-05-31 19:01:35 +0300

    homework 10_1 done add README.md add processing.py

--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
-from src.masks import get_mask_card_number, get_mask_account
-from src.widget import  mask_account_card, get_date
+from src.masks import get_mask_account, get_mask_card_number
+from src.processing import filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
 
 print("=" * 50)
 print("проверка get_mask_card_number")
@@ -44,3 +45,37 @@ for item in test_data:
     result = mask_account_card(item)
     print(f"{item:<35} -> {result}")
     # print(f"{item} -> {result}")
+
+test_list_dict = [
+    {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+    },
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+    },
+    {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+    },
+    {
+        "id": 615064591,
+        "state": "CANCELED",
+        "date": "2018-10-14T08:21:33.419441",
+    },
+]
+
+print("=" * 50)
+print("проверка filter_by_state")
+print("=" * 50)
+print(filter_by_state(test_list_dict, "EXECUTED"))
+
+print("=" * 50)
+print("проверка sort_by_date")
+print("=" * 50)
+print(sort_by_date(test_list_dict))
+print(sort_by_date(test_list_dict, False))
