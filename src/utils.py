3c54fd6e6136baa6bfac91d8a034e2d515b87679
svmikhaylov333 <svmikhaylov333@gmail.com>
2026-07-05 14:50:45 +0300

    добавлен модуль utils и файл данных operations.json

---
+++ src/utils.py
@@ -0,0 +1,27 @@
+import json
+import os
+from typing import List
+
+
+
+def load_operations(file_path: str) -> List:
+    """
+    Загружает транзакции из JSON-файла.
+    """
+    try:
+       # Проверка что файл существует и он не пустой"
+        if not os.path.exists(file_path):
+            return []
+        if os.path.getsize(file_path) == 0:
+            return []
+
+        # парисинг JSON Файла
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+            return data
+
+    except (json.JSONDecodeError, IOError, OSError):
+        # В случае любой ошибки возвращаем пустой список
+        return []
+
