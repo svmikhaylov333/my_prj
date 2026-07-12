bc8373087454163d8e80cb9c8a7ac63ca8169d15
svmikhaylov333 <svmikhaylov333@gmail.com>
2026-07-12 12:10:46 +0300

    добавлен модуль file_proccesing- функция чтения из csv файла

---
+++ src/file_processing.py
@@ -0,0 +1,30 @@
+import os
+from typing import List
+import pandas as pd
+
+# Модуль для считывания финансовых операций из CSV- и XLSX-файлов.
+def read_csv_operations(file_path: str) -> List:
+    """Функция для считывания финансовых операция из CSV файла"""
+
+    try:
+        # Проверка файла на существование и то что он не пустой
+        if not os.path.exists(file_path):
+            return []
+        if os.path.getsize(file_path) == 0:
+            return []
+        #Чтение файла CSV c разделителем ";"
+        df =pd.read_csv(file_path, sep=";")
+        operations = df.to_dict(orient="records")
+
+        # Замена NaN на None
+        for operation in operations:
+            for key, value in operation.items():
+                if pd.isna(value):
+                    operation[key] = None
+        return operations
+
+    except Exception:
+        #В случае любой ошибки возвращаем пустой список
+        return []
+
+
